c630b689a1d0c5ae1242bb73fee9c808974f399a
Nuclear03020704 <lahdjirayhan@gmail.com>
2021-05-16 19:38:50 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="pychatlib",
+    version="0.0.1",
+    description="Provides functionality to read messaging/chat application exports.",
+    url="https://github.com/lahdjirayhan/pychatlib",
+    license='BSD 3-clause',
+    author="Rayhan Lahdji",
+    packages=['chatlib'],
+    install_requires=['pydateinfer']
+)
