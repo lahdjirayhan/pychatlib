9758f11c51449d22eb526d4aa6c94ccbad336c70
Nuclear03020704 <lahdjirayhan@gmail.com>
2021-05-18 09:49:25 +0700

    Add docstring
    for majority of functions/methods. Also some type annotations.

--- chatlib/line.py
+++ chatlib/line.py
@@ -1,5 +1,6 @@
 import re
 from datetime import datetime
+from typing import Optional, Tuple
 from chatlib.date_tools import parse_or_none, infer_date
 from chatlib.base import BaseChatData
 
@@ -7,16 +8,28 @@ UNICODE_LS = "\u2028"
 UNICODE_PS = "\u2029"
 
 class LineChatData(BaseChatData):
-    def __init__(self, path, *args, **kwargs):
+    """Class to read and contain LINE chat export data"""
+    def __init__(self, path: str, *args, **kwargs):
+        """Create a LineChatData object using the provided path to export file
+
+        Args:
+            path (str): Path to export file.
+        """
         super().__init__(path, app_name = "LINE", *args, **kwargs)
     
-    def read_from_file(self, path, anonymize=False):
+    def read_from_file(self, path: str, anonymize: bool = False):
+        """Read chat data from file
+
+        Args:
+            path (str): Path to export file.
+            anonymize (bool, optional): If True, the chat data will be anonymized as good as possible. Defaults to False.
+        """
         self._date_time, self._sender, self._event, self._message = [], [], [], []
         with open(path, encoding="utf-8", errors="ignore") as f:
             self.room = f.readline().rstrip()
             self.export_time = f.readline().rstrip()
 
-            self._temp_date = None      # LINE uses hour as timestamps, dates are on different special row
+            self._temp_date = ""      # LINE uses hour as timestamps, dates are on different special row
             for line in f:
                 self.parse_row(line.rstrip())
         
@@ -65,7 +78,15 @@ class LineChatData(BaseChatData):
             $            # Ends (this is to prevent header-messages, it is quite popular i.e. [ANNOUNCEMENT])
         """, re.VERBOSE)
 
-    def parse_row(self, row):
+    def parse_row(self, row: str):
+        """Parse a single row of chat export file
+
+        Export files are read on a row-by-row basis. This function parses the information contained in each row
+        and determines whether a row is continuation for previous row.
+
+        Args:
+            row (str): String, a row from chat export file
+        """
         timestamp, rest_of_line = self._separate_timestamp(row)
         if not timestamp:
             # row does not contain timestamp
@@ -118,25 +139,47 @@ class LineChatData(BaseChatData):
             self._message[i] = message
 
 
-    def _separate_timestamp(self, line):
+    def _separate_timestamp(self, line: str) -> Tuple[Optional[str], str]:
         if not self.HOUR_PATTERN.match(line):
             return None, line
         
         hour, rest = line.split(self.SEP, 1)
         return hour, rest
     
-    def _separate_sender(self, line_segment):
+    def _separate_sender(self, line_segment: str) -> Tuple[Optional[str], str]:
         if self.SEP not in line_segment:
             return None, line_segment
         
         sender, message = line_segment.split(self.SEP, 1)
         return sender, message
     
-    def _record_continuation(self, line):
+    def _record_continuation(self, line: str):
+        """Record this line of string as a continuation row
+
+        Adds the current row to previous row's messsage
+
+        Args:
+            line (str): String, a line to be read into database
+        """
         if line:
             self._message[-1] += "\n" + line
 
     def _record_new_row(self, *args, **kwargs):
+        """Record this line of string as a new row
+
+        Adds the current row as a new row. New row requires a timestamp.
+
+        Args:
+            date_time (str): String, parseable into datetime, representing timestamp.
+            sender (str): Name of account involved with a record. Usually, sender of a chat.
+            event (str): Non-chat events, e.g. when someone joins a group or sends an image.
+            message (str): Message provided.
+        
+
+        Side note: when in doubt, it is advisable to resort to saving information in message.
+        This is because separation of event and message is not perfectly reliable, and
+        my style has been to default to message if unable to be sure if it is a non-chat event.
+        """
         self._date_time.append(kwargs["date_time"]) # For a new row, a timestamp must exist. Others may be None.
 
         self._sender.append(kwargs.get("sender"))
