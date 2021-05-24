9859ab1340a9e94b7c3184340e8211f46b8accdf
Nuclear03020704 <lahdjirayhan@gmail.com>
2021-05-24 20:39:49 +0700

    Update docs
    Now conf.py also takes version number from chatlib.__version__

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath(".."))
+import chatlib
 
 # -- Project information -----------------------------------------------------
 
@@ -22,7 +23,7 @@ copyright = '2021, Rayhan Lahdji'
 author = 'Rayhan Lahdji'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = chatlib.__version__
 
 
 # -- General configuration ---------------------------------------------------
